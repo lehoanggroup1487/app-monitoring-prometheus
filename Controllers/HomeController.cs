dfcade88b40a461d28ae0ae62bd9b4b129f5bfbe
phonglh <phonglh@evolableasia.vn>
2021-07-22 21:49:14 +0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApp.Models;
+using Prometheus;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // counter metrics
+        Counter counter = Metrics.CreateCounter("my_counter", "index page counter");
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // init counter
+            counter.Inc();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
